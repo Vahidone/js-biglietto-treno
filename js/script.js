91b44511351ebedf337ad9a95f0772ebe82759ea
Vahidone <v.ban.roma@gmail.com>
2023-09-07 16:45:27 +0200

    fix: fix result decimal

--- js/script.js
+++ js/script.js
@@ -8,13 +8,13 @@ const priceMinor = price * percentPriceMinor;
 const percentageSubOver = 40;
 const percentPriceOver = percentageSubOver / 100;
 const priceOver = price * percentPriceOver;
-let result;
+let message;
 const roundedResult = price.toFixed(2);
 
-if (age < 18) result = roundedResult - priceMinor;
-else if (age >= 65) result = roundedResult - priceOver;
-else result = roundedResult;
+if (age < 18) message = roundedResult - priceMinor;
+else if (age >= 65) message = roundedResult - priceOver;
+else message = roundedResult;
 
-const message = ` Il prezzo del tuo biglietto è € ${result}`;
+const result = ` Il prezzo del tuo biglietto è € ${message}`;
 
-document.getElementById("final-price").innerHTML = message;
+document.getElementById("final-price").innerHTML = result;
