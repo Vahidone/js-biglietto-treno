771dbf48e04c66f24f63a4f9acc829526ee8d37e
Vahidone <v.ban.roma@gmail.com>
2023-09-07 20:12:04 +0200

    perf: improve code in js

--- js/script.js
+++ js/script.js
@@ -11,8 +11,8 @@ const percentPriceOver = percentageSubOver / 100;
 const priceOver = priceTrip - percentPriceOver;
 let textBot = "Il prezzo del tuo biglietto è € ";
 let message;
-let verificaAge = Math.sign(age);
-let verificaKm = Math.sign(km);
+let verifyAge = Math.sign(age);
+let verifyKm = Math.sign(km);
 
 if (age < 18)
   message =
@@ -24,11 +24,11 @@ else message = textBot + priceTrip;
 
 // operazione di verifica **************
 if (age <= 2 && age > 0) message = "per te il biglietto è gratis :)";
-else if (verificaKm === 0 || verificaAge === 0)
+else if (verifyKm === 0 || verifyAge === 0)
   message = "Oops!!! non è possibile emettere il biglietto :(";
-else if (verificaAge === -1 || verificaAge === -1)
+else if (verifyKm === -1 || verifyAge === -1)
   message = "ATTENZIONE!! vengono accettate solo risposte reali !!";
-else if (verificaAge != 1 || verificaAge != 1)
+else if (verifyKm != 1 || verifyAge != 1)
   message = "ATTENZIONE!! non puoi lasciare nessun campo vuoto !!";
 
 document.getElementById("final-price").innerHTML = message;
